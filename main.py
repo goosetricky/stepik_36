27e622540672e52425b89e8261bf0703ea1bb27b
John Doe <johndoe@example.com>
2020-12-19 15:47:39 +0300

    task for test wait

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 link = "http://suninjuly.github.io/explicit_wait2.html"
 browser = webdriver.Chrome("C:/chromedriver.exe")
 browser.get(link)
-selector = "input_vaaluse"
+selector = "input_value"
 
 
 def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 
-print(True)
+b = browser.find_element_by_tag_name("#price")
+WebDriverWait(browser, 15).until(
+    expected_conditions.text_to_be_present_in_element((By.TAG_NAME, "#price"), "$100")
+)
+browser.find_element_by_tag_name("#book").click()
+x_element = browser.find_element_by_id(selector)
+x = x_element.text
+print(x_element.text)
+y = calc(x)
+browser.find_element_by_id("answer").send_keys(y)
+browser.find_element_by_tag_name("button.btn").click()
+#browser.find_element_by_id("solve").click()
+
 time.sleep(101)
 browser.quit()

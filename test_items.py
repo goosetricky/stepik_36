ca912abf678f1e98d22c176f487f139240626eb3
John Doe <johndoe@example.com>
2020-12-20 18:07:42 +0300

    add test_items for homework 3.6

---
+++ test_items.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+import math
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+# функция, определяющая наличие элемента на странице
+def is_exist_element(browser, selector_xpath):
+    try:
+        browser.find_element(By.XPATH, selector_xpath)
+        return True
+    except:
+        return False
+
+
+class TestMain:
+    def test_exist_button(self, browser):
+        # Ссылка на сайт
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+        # переходим на сайт
+        browser.get(link)
+
+        # определяем есть ли кнопка
+        assert is_exist_element(browser, "//form[@id='add_to_basket_form']//button"), "Кнопки нет на странице"
+
+        # Для проверки французской версии
+        time.sleep(3)

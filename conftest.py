dda940e8022d17e47a72a0b1f43ae39bff39d727
goosetricky <68857628+goosetricky@users.noreply.github.com>
2020-12-20 18:19:37 +0300

    edit confest for homework 3.6

--- conftest.py
+++ conftest.py
@@ -2,8 +2,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+
 
 
 def pytest_addoption(parser):
@@ -16,7 +15,7 @@ def browser(request):
     language = request.config.getoption("language")
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
